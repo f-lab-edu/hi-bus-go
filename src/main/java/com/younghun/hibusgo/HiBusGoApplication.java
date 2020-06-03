48cc40af02be75fd8bcb114ab55d16e0f8d533a3
joyounghun <whdudgns2654@gmail.com>
2020-06-03 22:50:51 +0900

    create project

---
+++ src/main/java/com/younghun/hibusgo/HiBusGoApplication.java
@@ -0,0 +1,13 @@
+package com.younghun.hibusgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiBusGoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiBusGoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/younghun/hibusgo/HiBusGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.younghun.hibusgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiBusGoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
