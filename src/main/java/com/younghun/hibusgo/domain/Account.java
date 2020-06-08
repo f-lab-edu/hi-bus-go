c55bb8da1dacb2ada884cc1ef9cfe41006306a38
joyounghun <whdudgns2654@gmail.com>
2020-06-08 15:11:22 +0900

    #2 회원가입 기능
    리뷰 내용 반영

--- src/main/java/com/younghun/hibusgo/domain/Account.java
+++ src/main/java/com/younghun/hibusgo/domain/Account.java
@@ -8,26 +8,19 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 @ToString
 public class Account {
 
     public enum Status { DEFAULT, DELETED}
 
-    // 아이디
     private String id;
 
-    // 패스워드
     private String password;
 
-    // 이름
     private String name;
 
-    // 이메일
     private String email;
 
-    // 핸드폰 번호
     private String phoneNumber;
 
     // 상태
