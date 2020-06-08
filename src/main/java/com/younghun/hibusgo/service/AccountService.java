72b65ef3959ee858bc54c7cce23b940045228e87
joyounghun <whdudgns2654@gmail.com>
2020-06-08 14:33:38 +0900

    #2 회원가입 기능
    리뷰 내용 반용

--- src/main/java/com/younghun/hibusgo/service/AccountService.java
+++ src/main/java/com/younghun/hibusgo/service/AccountService.java
@@ -8,14 +8,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 @RequiredArgsConstructor
 @Log4j2
 public class AccountService {
 
     private final AccountMapper accountMapper;
 
-    @Transactional(readOnly = true)
     public Account findById(String id) {
         Account account = accountMapper.findById(id);
         return account;
