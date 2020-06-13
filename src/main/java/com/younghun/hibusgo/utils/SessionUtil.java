13b55813916e6b1549395984a79eae74ea78acbb
joyounghun <whdudgns2654@gmail.com>
2020-06-13 22:48:04 +0900

    #3 로그인 기능
    리뷰 반영
    
    SessionUtil 주석 추가

--- src/main/java/com/younghun/hibusgo/utils/SessionUtil.java
+++ src/main/java/com/younghun/hibusgo/utils/SessionUtil.java
@@ -6,10 +6,21 @@ public class SessionUtil {
 
   public static final String ACCOUNT_MEMBER_ID = "ACCOUNT_MEMBER_ID";
 
+  /**
+   * 계정 로그인 메소드
+   * @param session
+   * @param accountId
+   * 계정 로그인 id를 세션으로 등록
+   */
   public static void accountLogin(HttpSession session, String accountId) {
     session.setAttribute(ACCOUNT_MEMBER_ID, accountId);
   }
 
+  /**
+   * 계정 로그아웃 메소드
+   * @param session
+   * 계정 로그인 id 세션 정보 삭제
+   */
   public static void accountLogout(HttpSession session) {
     session.removeAttribute(ACCOUNT_MEMBER_ID);
   }
