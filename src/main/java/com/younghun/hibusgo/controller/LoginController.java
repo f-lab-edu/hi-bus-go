e6baff8356bc888a99ad096ab0c49334e4ba1493
joyounghun <whdudgns2654@gmail.com>
2020-08-03 19:44:01 +0900

    #12 로그인 status 체크 로직 수정
    회원 검색시 status 체크를 자바 로직에서 체크
    탈퇴된 회원 검색이 추후 필요 할수도 있으므로
    로그인 실패시 404 code return

--- src/main/java/com/younghun/hibusgo/controller/LoginController.java
+++ src/main/java/com/younghun/hibusgo/controller/LoginController.java
@@ -2,11 +2,12 @@ package com.younghun.hibusgo.controller;
 
 
 
-import static com.younghun.hibusgo.utils.ResponseConstants.RESPONSE_ENTITY_NO_CONTENT;
 import static com.younghun.hibusgo.utils.ResponseConstants.RESPONSE_ENTITY_OK;
+import static com.younghun.hibusgo.utils.ResponseConstants.RESPONSE_NOT_FOUND;
 
 import com.younghun.hibusgo.aop.LoginCheck;
 import com.younghun.hibusgo.domain.Account;
+import com.younghun.hibusgo.domain.Account.Status;
 import com.younghun.hibusgo.dto.LoginDto;
 import com.younghun.hibusgo.service.AccountService;
 import com.younghun.hibusgo.service.LoginService;
@@ -59,7 +60,7 @@ public class LoginController {
      *
      * @param loginDto
      * @return 로그인 성공시 200 code return
-     * 로그인 실패시 정상이지만 데이터가 없음을 의미하는 204 code return
+     * 로그인 실패시 데이터가 없음을 의미하는 404 code return
      */
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @NotNull LoginDto loginDto) {
@@ -68,8 +69,8 @@ public class LoginController {
 
         Account account = accountService.findByIdAndPassword(accountId, password);
 
-        if (account == null) {
-            return RESPONSE_ENTITY_NO_CONTENT;
+        if (account == null || account.getStatus().equals(Status.DELETED)) {
+            return RESPONSE_NOT_FOUND;
         }
 
         loginService.accountLogin(account.getId());
