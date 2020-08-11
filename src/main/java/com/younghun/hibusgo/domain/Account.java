1165371edab7af63d3427e95d740e4d9e61f1aa3
joyounghun <whdudgns2654@gmail.com>
2020-08-12 00:38:50 +0900

    #18 패스워드 인코딩
    Account 클래스에서 사용하지 않는 PasswordEncoder import 제거

--- src/main/java/com/younghun/hibusgo/domain/Account.java
+++ src/main/java/com/younghun/hibusgo/domain/Account.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Getter
 @Builder
