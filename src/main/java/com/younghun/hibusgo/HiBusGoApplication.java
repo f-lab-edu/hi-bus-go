8070d3463fcfb639bd09a4510ff800df476f24f9
joyounghun <whdudgns2654@gmail.com>
2020-08-14 22:34:30 +0900

    #22 지역 조회 캐쉬 적용
    지역 조회시 캐시 적용하는 어노테이션 추가

--- src/main/java/com/younghun/hibusgo/HiBusGoApplication.java
+++ src/main/java/com/younghun/hibusgo/HiBusGoApplication.java
@@ -2,7 +2,12 @@ package com.younghun.hibusgo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+/**
+ * @EnableCaching : Application에게 Cache를 사용하다는 의미로 사용
+ */
+@EnableCaching
 @SpringBootApplication
 public class HiBusGoApplication {
 
