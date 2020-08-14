e70338ab6df4dda07643425c9ac8dbadf2eb43bd
joyounghun <whdudgns2654@gmail.com>
2020-08-14 22:30:01 +0900

    #20 버스터미널 조회 캐쉬 적용
    버스터미널 조회시 캐시 적용하는 어노테이션 추가

--- src/main/java/com/younghun/hibusgo/controller/BusTerminalController.java
+++ src/main/java/com/younghun/hibusgo/controller/BusTerminalController.java
@@ -7,6 +7,7 @@ import com.younghun.hibusgo.service.BusTerminalService;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -31,6 +32,11 @@ public class BusTerminalController {
    * @param name 터미널 이름
    * @return ResponseEntity(성공시 204 code, 실패시 204 code)
    */
+  /**
+   * @Cacheable : 동일 값이 Cache에 있는 경우 Cache에서 데이터를 return합니다.
+   * 만약 동일 key 값이 없을 경우 메소드를 실행하고 반환된 결과 값을 Cache에 저장합니다.
+   */
+  @Cacheable(value = "terminals", key = "#name")
   @GetMapping("/{region}/{name}")
   public ResponseEntity<?> getBusTerminal(@PathVariable String region, @PathVariable String name) {
     Optional<BusTerminal> busTerminal = busTerminalService.findByNameAndRegion(name, region);
