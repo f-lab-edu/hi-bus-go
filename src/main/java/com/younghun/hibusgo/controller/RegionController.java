308662f3bc85c565243e64d05e199d328184ad1a
joyounghun <whdudgns2654@gmail.com>
2020-08-18 21:20:05 +0900

    #24 지역 조회시 redis에 ResponseEntity 저장하는것이 아닌 지역 클래스로 저장하도록 수정
    redis에 저장하는할때는 ResponseEntity가 아닌 해당 객체로 저장을 해야하므로 수정

--- src/main/java/com/younghun/hibusgo/controller/RegionController.java
+++ src/main/java/com/younghun/hibusgo/controller/RegionController.java
@@ -5,6 +5,7 @@ import static com.younghun.hibusgo.utils.ResponseConstants.RESPONSE_ENTITY_NO_CO
 import com.younghun.hibusgo.domain.Region;
 import com.younghun.hibusgo.service.RegionService;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.cache.annotation.Cacheable;
@@ -23,27 +24,19 @@ public class RegionController {
   private final RegionService regionService;
 
   /**
-   * 지 조회 메서드
-   * 조회후 값이 없으면 컨텐츠를 리턴하지 않음을 의미하는 204 code return
-   * 값이 있으면 ResponseEntity Body에 Region return
+   * 지역 조회 메서드
    * @param name 조회할 지역 이름
-   * @return ResponseEntity
+   * @return List<Region>
    */
   /**
    * @Cacheable : 동일 값이 Cache에 있는 경우 Cache에서 데이터를 return합니다.
    * 만약 동일 key 값이 없을 경우 메소드를 실행하고 반환된 결과 값을 Cache에 저장합니다.
    */
-
-  @Cacheable(value = "regions", key = "#name", cacheManager = "redisCacheManager")
+  @Cacheable(value = "regions.name", key = "#name", cacheManager = "redisCacheManager")
   @GetMapping("/{name}")
-  public ResponseEntity getRegion(@PathVariable String name) {
-    List<Region> regions = regionService.searchByName(name);
-
-    if (regions == null || regions.isEmpty()) {
-      return RESPONSE_ENTITY_NO_CONTENT;
-    }
-
-    return ResponseEntity.ok().body(regions);
+  public List<Region> getRegion(@PathVariable String name) {
+    Optional<List<Region>> regions = regionService.searchByName(name);
+    return regions.get();
   }
 
 }
--- src/main/java/com/younghun/hibusgo/service/RegionService.java
+++ src/main/java/com/younghun/hibusgo/service/RegionService.java
@@ -3,6 +3,7 @@ package com.younghun.hibusgo.service;
 import com.younghun.hibusgo.domain.Region;
 import com.younghun.hibusgo.mapper.RegionMapper;
 import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
@@ -18,7 +19,7 @@ public class RegionService {
     return regionMapper.findById(id);
   }
 
-  public List<Region> searchByName(String name) {
-    return regionMapper.searchByName(name);
+  public Optional<List<Region>> searchByName(String name) {
+    return Optional.ofNullable(regionMapper.searchByName(name));
   }
 }
