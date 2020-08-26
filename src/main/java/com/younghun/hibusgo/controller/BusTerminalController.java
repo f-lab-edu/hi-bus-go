3422f21f747f745b84b8dbfb2aede4e7fe186d3a
joyounghun <whdudgns2654@gmail.com>
2020-08-26 22:03:05 +0900

    #33 리뷰 반영
    터미널을 수정하는것이므로 post보단 patch를 사용하도록 수정

--- src/main/java/com/younghun/hibusgo/controller/BusTerminalController.java
+++ src/main/java/com/younghun/hibusgo/controller/BusTerminalController.java
@@ -20,6 +20,7 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -126,7 +127,7 @@ public class BusTerminalController {
    * @return ResponseEntity
    */
   @LoginCheck(userLevel = UserLevel.ADMIN)
-  @PostMapping()
+  @PatchMapping()
   public ResponseEntity<?> updateBusTerminal(@RequestBody @Valid BusTerminalDto busTerminalDto) {
     String name = busTerminalDto.getName();
     int regionId = busTerminalDto.getRegionId();
