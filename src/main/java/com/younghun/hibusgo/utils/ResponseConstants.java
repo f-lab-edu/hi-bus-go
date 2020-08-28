afc1e1bd2de21f0c002201d1b45e26417f0bcf0f
joyounghun <whdudgns2654@gmail.com>
2020-08-28 21:56:00 +0900

    #33 리뷰 반영
    RESPONSE_REGION_BAD_REQUEST의 body가 String타입에 맞게 제네릭 타입 변경

--- src/main/java/com/younghun/hibusgo/utils/ResponseConstants.java
+++ src/main/java/com/younghun/hibusgo/utils/ResponseConstants.java
@@ -17,5 +17,5 @@ public class ResponseConstants {
   public static final ResponseEntity<Void> RESPONSE_ENTITY_OK = ResponseEntity.ok().build();
   public static final ResponseEntity<Void> RESPONSE_NOT_FOUND = ResponseEntity.notFound().build();
   public static final ResponseEntity<Void> RESPONSE_CONFLICT = ResponseEntity.status(HttpStatus.CONFLICT).build();
-  public static final ResponseEntity<?> RESPONSE_REGION_BAD_REQUEST = ResponseEntity.badRequest().body("이미 삭제된 지역이거나, 잘못된 지역 입니다.");
+  public static final ResponseEntity<String> RESPONSE_REGION_BAD_REQUEST = ResponseEntity.badRequest().body("이미 삭제된 지역이거나, 잘못된 지역 입니다.");
 }
