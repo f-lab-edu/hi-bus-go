1e5790fa2faa89f7836c09bf60780bf881e00298
joyounghun <whdudgns2654@gmail.com>
2020-09-03 17:47:19 +0900

    #39 로그인 아이디를 가져오는 어노테이션 이름 변경(@AccountId -> @LoginUserId)
    로그인한 유저의 아이디를 가져온다는 의미를 명확하게 하기 위해 LoginUserId로 이름 변경

--- src/main/java/com/younghun/hibusgo/utils/LoginUserIddResolver.java
+++ src/main/java/com/younghun/hibusgo/utils/LoginUserIddResolver.java
@@ -10,30 +10,30 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.method.support.ModelAndViewContainer;
 
 /**
- * AccountId 어노테이션을 파라미터에 사용시 값을 바인딩하는 클래스
+ * LoginUserId 어노테이션을 파라미터에 사용시 값을 바인딩하는 클래스
  * @author 조영훈
  * <p>HandlerMethodArgumentResolver는 특정 조건에 맞는 파라미터가 있을때,
  * 원하는 값을 바인딩 해주는 인터페이스</p>
  */
 @Component
 @RequiredArgsConstructor
-public class AccountIdResolver implements HandlerMethodArgumentResolver {
+public class LoginUserIddResolver implements HandlerMethodArgumentResolver {
 
   private final LoginService loginService;
 
   /**
-   * AccountId 어노테이션 파라미터로 등록하는 메소드
+   * LoginUserId 어노테이션 파라미터로 등록하는 메소드
    *
    * @param parameter
    * @return boolean
    */
   @Override
   public boolean supportsParameter(MethodParameter parameter) {
-    return parameter.hasMethodAnnotation(AccountId.class);
+    return parameter.hasMethodAnnotation(LoginUserId.class);
   }
 
   /**
-   * SessionId 어노테이션 파라미터에 사용시, 사용자의 아이디를 리턴하는 메소드
+   * LoginUserId 어노테이션 파라미터에 사용시, 사용자의 아이디를 리턴하는 메소드
    *
    * @return Object(사용자의 아이디)
    */
