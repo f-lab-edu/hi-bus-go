47b4eb339acb1ddf5b47b174d53fda87929663ee
joyounghun <whdudgns2654@gmail.com>
2020-09-24 18:10:02 +0900

    #58 결제 수단에 추상 클래스를 통한 다형성 적용
    추상 클래스를 활용하여 결제 수단에서 공통 되는 변수들을 한곳에서 모으고
    다른 부분은 각자 클래스에서 사용하도록 변경

--- src/main/java/com/younghun/hibusgo/domain/KakaoPay.java
+++ src/main/java/com/younghun/hibusgo/domain/KakaoPay.java
@@ -1,34 +1,17 @@
 package com.younghun.hibusgo.domain;
 
-import java.time.LocalDateTime;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.ToString;
+import lombok.experimental.SuperBuilder;
 
 @Getter
-@Builder
+@SuperBuilder
 @ToString
-public class KakaoPay implements PaymentMeans {
-
-  // 결제 수단 아이디
-  private long id;
-
-  // 결제 아이디
-  private long paymentId;
+public class KakaoPay extends PaymentMeans {
 
   // 카드 번호
   private String cardNumber;
 
   // 카드 회사 이름
   private String cardName;
-
-  // 상태
-  private DataStatus status;
-
-  // 결제 수단 추가일
-  private LocalDateTime createdAt;
-
-  // 결제 수단 수정일
-  private LocalDateTime updatedAt;
-
 }
